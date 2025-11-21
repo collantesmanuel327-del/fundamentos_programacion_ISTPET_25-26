91456d2c24198f8b7c5ed2d5877b8dd1ba69ae81
Manuel Collantes <collantesmanuel327@gmail.com>
2025-11-21 10:07:23 -0500

    2.0

--- Variable/src/Variable/Division.java
+++ Variable/src/Variable/Division.java
@@ -4,13 +4,13 @@ public class Division {
 
 	public static void main(String[] args) {
 		float Numero1 = 50;
-		float Numero2 = 5;
-		
+		float Numero2 = 0;
+	    if (Numero2!=0) {
 		float Division = (Numero1/Numero2);
-		
-		System.out.printf("La division de los dos numeros es:" + Division);
-				
-
+		System.out.printf("La division de los dos numeros es: " + Division);
+	    }else {
+	    	System.out.print("No se puede dividir para 0");
+	    }
 	}
 
 }
