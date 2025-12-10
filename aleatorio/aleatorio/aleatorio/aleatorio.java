6fd1e3887e657e92c0c9dd6075019ad4f99749d5
Manuel Collantes <collantesmanuel327@gmail.com>
2025-12-09 23:24:13 -0500

    Aleatorio

---
+++ aleatorio/aleatorio/aleatorio/aleatorio.java
@@ -0,0 +1,34 @@
+package aleatorio;
+
+import java.util.Scanner;
+
+public class aleatorio {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int c = 0;
+		int r = (int) (Math.random()*100+1);
+		while (true) {
+			c=c+1;
+			System.out.print("Ingrese el numero generado: ");
+			Integer n= sc.nextInt();
+			if(n<r) {
+				System.out.print("Es menor al generado: ");
+			}
+			if (n>r) {
+				System.out.print("Es mayor al generado:");
+			}
+			if (n==r) {
+				System.out.print("Correcto: ");
+				break;
+			}
+			if (c==10) {
+				System.out.println("Que pena: ");
+				break;
+			}
+		}
+		System.out.println("\nLas veces que lo intento fueron: " + c);
+
+	}
+
+}
